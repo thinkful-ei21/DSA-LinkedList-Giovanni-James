bcf75a2628eb41a46aacd96827729b20d2e43b55
giovanni <mormagli@gmail.com>
2018-07-26 11:28:05 -0500

    insertAt implimented

--- linkedlist.js
+++ linkedlist.js
@@ -1,46 +1,57 @@
 'use strict';
 
 class LinkedList {
-    constructor() {
-        this.head = null;
+  constructor() {
+    this.head = null;
+  }
+
+  insertFirst(item) {
+    this.head = new Node(item, this.head);
+  }
+
+  insertLast(item) {
+    //check if list is empty
+    // if it is, insert the item as the only item in the list
+
+    if (this.head === null) {
+      this.head = new Node(item, this.head);
     }
 
-    insertFirst(item) {
-        this.head = new Node(item, this.head);
+    let currentNode = this.head;
+    while (currentNode.next !== null) {
+      currentNode = currentNode.next;
     }
 
-    insertLast(item) {
-        //check if list is empty
-        // if it is, insert the item as the only item in the list
+    currentNode.next = new Node(item, null);
+  }
+  
+  insertAt(item, index){
 
-        if (this.head === null) {
-            this.head = new Node(item, this.head);
-        }
+    let nodeIndex = 0;
+    
+    let currentNode = this.head;
 
-        let currentNode = this.head;
-        while (currentNode.next !== null) {
-            currentNode = currentNode.next;
-        }
-
-        currentNode.next = new Node(item, null);
+    while( nodeIndex +1 !== index){
+      if(currentNode === null){return new Error('index out of range');}
+      
+      currentNode = currentNode.next;
+      nodeIndex ++;
     }
-    //   insertAt(item, index=0){
 
-    //     let nodeIndex = 0;
-    //     let node = this.head;
-    //     while( nodeIndex -1 !== index){
-    //       node
-    //       nodeIndex ++
-    //     }
+    //now that we've targeted the node *before* our insertion, we'll save the 'next' value
+    let nextNode = currentNode.next;
 
-    //   }
+    //here we use that stored 'next' value to point to the node *after* our insertion
+    currentNode.next = new Node(item, nextNode);
+
+  }
 }
 
 class Node {
-    constructor(value, next) {
-        this.next = next;
-        this.value = value;
-    }
+  constructor(value, next) {
+    this.next = next;
+    this.value = value;
+  }
 }
 
 module.exports = LinkedList;
